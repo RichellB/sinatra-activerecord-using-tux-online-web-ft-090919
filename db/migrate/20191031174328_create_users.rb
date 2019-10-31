c89eed2c69453fcd21fd5108c8b4ebd81ede1732
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-31 17:43:59 +0000

    Automatically backed up by Learn

--- db/migrate/20191031174328_create_users.rb
+++ db/migrate/20191031174328_create_users.rb
@@ -1,4 +1,9 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[4.2.0]
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :fav_icecream
+    end
   end
 end
